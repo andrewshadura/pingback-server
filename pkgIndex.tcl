a23fd3fd75348341becf279167d9a6ef8b9f75bc
Andrej Shadura <andrew@shadura.me>
2015-03-06 11:36:15 +0100

    Add pkgIndex

---
+++ pkgIndex.tcl
@@ -0,0 +1,4 @@
+# package index for wibble
+if {![package vsatisfies [package provide Tcl] 8.6]} {error "Tcl 8.6 is required"}
+package ifneeded wibble 0.1 [list source [file join $dir wibble.tcl]]
+package ifneeded xmlrpc 0.0 [list source [file join $dir xmlrpc.tcl]]
